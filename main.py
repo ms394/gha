31b1c9cc703b52644886de394affb7e6ba8ea3b8
Mohsin Sajan <39456333+ms394@users.noreply.github.com>
2024-07-29 14:18:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    if len(sys.argv) != 5:
+        logger.info(f"Usage: python {sys.argv[0]} <source_project_name> <source_sa_client_secret> <target_project_name> <target_sa_client_secret>")
+        return
+
+    print(sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+
+    return
+
+
+
+
+if __name__ == "__main__":
+    main()
