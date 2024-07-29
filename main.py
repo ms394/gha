21df17b3d78486916630b672bd30e06d72ff1724
Mohsin Sajan <39456333+ms394@users.noreply.github.com>
2024-07-29 15:45:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
         logger.info(f"Usage: python {sys.argv[0]} <source_project_name> <source_sa_client_secret> <target_project_name> <target_sa_client_secret>")
         return
     print('python')
-    print("python: "sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    print("python: ",sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 
     return
 
